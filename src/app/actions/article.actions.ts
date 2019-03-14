743968faabc2dc86baf5bf8becd50e667ed7c967
hemantkumar450 <38126340+hemantkumar450@users.noreply.github.com>
2019-03-14 15:31:22 +0530

    Update article.actions.ts

--- src/app/actions/article.actions.ts
+++ src/app/actions/article.actions.ts
@@ -2,8 +2,20 @@ import { Action } from '@ngrx/store';
 import { Article } from '../models/article';
 
 export const JAVA = 'Java';
-export const  ANGULAR = 'Angular';
-export const  MY_ARTICLES = 'Favorite_Articles';
+export const ANGULAR = 'Angular';
+export const MY_ARTICLES = 'Favorite_Articles';
+export const ADD_ARTICLE = '[TUTORIAL] Add';
+export const REMOVE_ARTICLE = '[TUTORIAL] Remove';
+
+export class AddArticle implements Action {
+  readonly type = ADD_ARTICLE
+  constructor(public payload: Article[]) { }
+}
+
+export class RemoveArticle implements Action {
+  readonly type = REMOVE_ARTICLE
+  constructor(public payload: number) { }
+}
 
 export class JavaArticlesAction implements Action {
   readonly type = JAVA;
@@ -16,7 +28,7 @@ export class AngularArticlesAction implements Action {
 export class FavoriteArticlesAction implements Action {
   readonly type = MY_ARTICLES;
 
-  constructor(public payload: Article[]) {}
+  constructor(public payload: Article[]) { }
 }
 
-export type All = JavaArticlesAction | AngularArticlesAction | FavoriteArticlesAction; 
+export type All = JavaArticlesAction | AngularArticlesAction | FavoriteArticlesAction | AddArticle | RemoveArticle; 
