aea23409ee5bb1aac1bb86bdb970773f9ce5f288
hemantkumar450 <38126340+hemantkumar450@users.noreply.github.com>
2019-03-14 15:29:53 +0530

    Update article.reducer.ts

--- src/app/reducers/article.reducer.ts
+++ src/app/reducers/article.reducer.ts
@@ -3,29 +3,35 @@ import * as fromActions from '../actions/article.actions';
 import { JAVA_ARTICLES, ANGULAR_ARTICLES } from '../models/article';
 import { ArticleState } from './app.states';
 
-export const initialState: ArticleState = { articles: []};
+export const initialState: ArticleState = { articles: [{ id: 1, title: 'test', category: 'test' }] };
 
-export function reducer(state = initialState, action: fromActions.All): ArticleState {
-  switch(action.type) {
+export function reducer(state: ArticleState = initialState, action: fromActions.All): ArticleState {
+  switch (action.type) {
     case fromActions.JAVA: {
-      return {articles: JAVA_ARTICLES};
+      return { articles: JAVA_ARTICLES };
     }
     case fromActions.ANGULAR: {
-      return {articles: ANGULAR_ARTICLES};
-    
-    } 
+      return { articles: ANGULAR_ARTICLES };
+    }
     case fromActions.MY_ARTICLES: {
-      return {articles: action.payload};
-    }    
+      return { articles: action.payload };
+    }
+    case fromActions.ADD_ARTICLE: {
+      state.articles = [...state.articles, ...action.payload]
+      return { articles: state.articles };
+    }
+    case fromActions.REMOVE_ARTICLE: {
+      return { articles: ANGULAR_ARTICLES };
+    }
     default: {
       return state;
     }
-  }	
+  }
 }
 
 export const getArticleState = createFeatureSelector<ArticleState>('articleState');
 
 export const getArticles = createSelector(
-    getArticleState, 
-    (state: ArticleState) => state.articles 
+  getArticleState,
+  (state: ArticleState) => state.articles
 );
